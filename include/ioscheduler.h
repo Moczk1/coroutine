9d4d0556541a35ca6a7461e483eaa289cf6a7997
moczkrin <moczkrin@outlook.com>
2026-07-17 18:27:27 +0800

    add pragma

--- include/ioscheduler.h
+++ include/ioscheduler.h
@@ -1,5 +1,6 @@
-#include "scheduler.h"
+#pragma once
 #include "fiber.h"
+#include "scheduler.h"
 #include "timer.h"
 
 #include <atomic>
@@ -7,118 +8,119 @@
 
 namespace moczkrin
 {
-    class IOManager : public Scheduler, public TimerManager
-    {
-    public:
-        enum Event
-        {
-            NONE = 0x000,
-            READ = 0x001, // 数值上正好等于 EPOLLIN
-            WRITE = 0x004 // 数值上正好等于 EPOLLOUT
-        };
+class IOManager : public Scheduler, public TimerManager
+{
+  public:
+	enum Event
+	{
+		NONE = 0x000,
+		READ = 0x001, // 数值上正好等于 EPOLLIN
+		WRITE = 0x004 // 数值上正好等于 EPOLLOUT
+	};
 
-    private:
-        struct FdContext
-        {
-            struct EventContext
-            {
-                Scheduler *scheduler = nullptr;
-                std::function<void()> cb;
-                std::shared_ptr<Fiber> fiber_ptr;
-            };
+  private:
+	struct FdContext
+	{
+		struct EventContext
+		{
+			Scheduler *scheduler = nullptr;
+			std::function<void()> cb;
+			std::shared_ptr<Fiber> fiber_ptr;
+		};
 
-            EventContext read;
-            EventContext write;
+		EventContext read;
+		EventContext write;
 
-            int fd = 0;
+		int fd = 0;
 
-            // enum 表示 任务的具体事件类型。
-            // 用 bit 分别表示不同的类型，所以同一fd可能有多种事件
-            Event events = NONE;
+		// enum 表示 任务的具体事件类型。
+		// 用 bit 分别表示不同的类型，所以同一fd可能有多种事件
+		Event events = NONE;
 
-            std::mutex mutex;
+		std::mutex mutex;
 
-            EventContext &getEventContext(Event event)
-            {
-                assert(event == READ || event == WRITE);
-                switch (event)
-                {
-                case READ:
-                    return read;
-                case WRITE:
-                    return write;
-                }
-                throw std::invalid_argument("Unsupported event type");
-            }
+		EventContext &getEventContext(Event event)
+		{
+			assert(event == READ || event == WRITE);
+			switch (event)
+			{
+			case READ:
+				return read;
+			case WRITE:
+				return write;
+			}
+			throw std::invalid_argument("Unsupported event type");
+		}
 
-            void resetEventContext(EventContext &eventContext)
-            {
-                eventContext.cb = nullptr;
-                eventContext.scheduler = nullptr;
-                eventContext.fiber_ptr.reset();
-            }
+		void resetEventContext(EventContext &eventContext)
+		{
+			eventContext.cb = nullptr;
+			eventContext.scheduler = nullptr;
+			eventContext.fiber_ptr.reset();
+		}
 
-            void triggerEvent(Event event)
-            {
-                assert(events & event);
-                events = (Event)(events & ~event);
-                EventContext eventContext = getEventContext(event);
-                if (eventContext.cb)
-                {
-                    eventContext.scheduler->scheduleLock(eventContext.cb);
-                }
-                else if (eventContext.fiber_ptr)
-                {
-                    eventContext.scheduler->scheduleLock(eventContext.fiber_ptr);
-                }
-                resetEventContext(eventContext);
-            }
-        };
+		void triggerEvent(Event event)
+		{
+			assert(events & event);
+			events = (Event)(events & ~event);
+			EventContext eventContext = getEventContext(event);
+			if (eventContext.cb)
+			{
+				eventContext.scheduler->scheduleLock(eventContext.cb);
+			}
+			else if (eventContext.fiber_ptr)
+			{
+				eventContext.scheduler->scheduleLock(eventContext.fiber_ptr);
+			}
+			resetEventContext(eventContext);
+		}
+	};
 
-    public:
-        IOManager(size_t threads = 1, bool use_caller = true, const std::string &name = "IOManager");
-        ~IOManager();
+  public:
+	IOManager(size_t threads = 1, bool use_caller = true,
+	    const std::string &name = "IOManager");
+	~IOManager();
 
-        /**
-         * 暴露四种外部调用
-         */
-        // add one event at a time
-        int addEvent(int fd, Event event, std::function<void()> cb = nullptr);
-        // delete event
-        bool delEvent(int fd, Event event);
-        // delete the event and trigger its callback
-        bool cancelEvent(int fd, Event event);
-        // delete all events and trigger its callback
-        bool cancelAll(int fd);
+	/**
+	 * 暴露四种外部调用
+	 */
+	// add one event at a time
+	int addEvent(int fd, Event event, std::function<void()> cb = nullptr);
+	// delete event
+	bool delEvent(int fd, Event event);
+	// delete the event and trigger its callback
+	bool cancelEvent(int fd, Event event);
+	// delete all events and trigger its callback
+	bool cancelAll(int fd);
 
-        static IOManager *GetThis();
+	static IOManager *GetThis();
 
-    protected:
-        void tickle() override;
+  protected:
+	void tickle() override;
 
-        bool stopping() override;
+	bool stopping() override;
 
-        void idle() override;
+	void idle() override;
 
-        void onTimerInsertedAtFront() override;
+	void onTimerInsertedAtFront() override;
 
-        void contextResize(size_t size);
+	void contextResize(size_t size);
 
-        /**
-         * 成员变量全部私有
-         */
-    private:
-        int m_epfd = 0;
-        // 读写管道
-        int m_pip[2] = {0};
+	/**
+	 * 成员变量全部私有
+	 */
+  private:
+	int m_epfd = 0;
+	// 读写管道
+	int m_pip[2] = {0};
 
-        // 原子变量计数
-        std::atomic<uint64_t> m_pendingEventsCount = {0};
+	// 原子变量计数
+	std::atomic<uint64_t> m_pendingEventsCount = {0};
 
-        // 多线程访问管理集合
-        std::shared_mutex m_mutex;
+	// 多线程访问管理集合
+	std::shared_mutex m_mutex;
 
-        // FdContexts 管理集合
-        std::vector<FdContext *> m_fdContexts;
-    };
+	// FdContexts 管理集合
+	std::vector<FdContext *> m_fdContexts;
 };
+}; // namespace moczkrin
--- include/timer.h
+++ include/timer.h
@@ -1,82 +1,90 @@
-#include <memory>
-#include <vector>
-#include <set>
-#include <shared_mutex>
+#pragma once
+
 #include <assert.h>
 #include <functional>
+#include <memory>
 #include <mutex>
+#include <set>
+#include <shared_mutex>
+#include <vector>
 
 namespace moczkrin
 {
-    class TimerManager;
-    class Timer : public std::enable_shared_from_this<Timer>
-    {
-        friend class TimerManager;
+class TimerManager;
+class Timer : public std::enable_shared_from_this<Timer>
+{
+	friend class TimerManager;
 
-    private:
-        bool m_recurring = false;
-        uint64_t m_timespan;
-        std::chrono::time_point<std::chrono::system_clock> m_nextTimePoint;
-        std::function<void()> m_cb;
-        TimerManager *m_manager;
+  private:
+	bool m_recurring = false;
+	uint64_t m_timespan;
+	std::chrono::time_point<std::chrono::system_clock> m_nextTimePoint;
+	std::function<void()> m_cb;
+	TimerManager *m_manager;
 
-        struct Comparator
-        {
-            bool operator()(const std::shared_ptr<Timer> &t1, const std::shared_ptr<Timer> &t2) const
-            {
-                assert(t1 != t2);
-                return t1->m_nextTimePoint < t2->m_nextTimePoint;
-            }
-        };
+	struct Comparator
+	{
+		bool operator()(const std::shared_ptr<Timer> &t1,
+		    const std::shared_ptr<Timer> &t2) const
+		{
+			assert(t1 != t2);
+			return t1->m_nextTimePoint < t2->m_nextTimePoint;
+		}
+	};
 
-    public:
-        bool cancel();
-        bool refresh();
-        bool reset(uint64_t timespan, bool from_now);
+  public:
+	bool cancel();
+	bool refresh();
+	bool reset(uint64_t timespan, bool from_now);
 
-    private:
-        Timer(uint64_t ms, std::function<void()> cb, bool recurring, TimerManager *manager);
-    };
+  private:
+	Timer(uint64_t ms, std::function<void()> cb, bool recurring,
+	    TimerManager *manager);
+};
 
-    class TimerManager
-    {
-        friend class Timer;
+class TimerManager
+{
+	friend class Timer;
 
-    public:
-        TimerManager();
-        virtual ~TimerManager();
-        std::shared_ptr<Timer> addTimer(uint64_t ms, std::function<void()> cb, bool recurring = false);
+  public:
+	TimerManager();
+	virtual ~TimerManager();
+	std::shared_ptr<Timer> addTimer(
+	    uint64_t ms, std::function<void()> cb, bool recurring = false);
 
-        std::shared_ptr<Timer> addConditionTimer(uint64_t ms, std::function<void()> cb, std::weak_ptr<void> weak_cond, bool recurring = false);
+	std::shared_ptr<Timer> addConditionTimer(uint64_t ms,
+	    std::function<void()> cb, std::weak_ptr<void> weak_cond,
+	    bool recurring = false);
 
-        // 拿到堆中最近的超时时间
-        uint64_t getNextTimer();
+	// 拿到堆中最近的超时时间
+	uint64_t getNextTimer();
 
-        // 取出所有超时定时器的回调函数
-        void listExpiredCb(std::vector<std::function<void()>> &cbs);
+	// 取出所有超时定时器的回调函数
+	void listExpiredCb(std::vector<std::function<void()>> &cbs);
 
-        // 堆中是否有timer
-        bool hasTimer() ;
+	// 堆中是否有timer
+	bool hasTimer();
 
-    protected:
-        // 当一个最早的timer加入到堆中 -> 调用该函数
-        virtual void onTimerInsertedAtFront() {};
+  protected:
+	// 当一个最早的timer加入到堆中 -> 调用该函数
+	virtual void onTimerInsertedAtFront() {};
 
-        // 添加timer
-        void addTimer(std::shared_ptr<Timer> timer);
+	// 添加timer
+	void addTimer(std::shared_ptr<Timer> timer);
 
-    private:
-        // 当系统时间改变时 -> 调用该函数
-        bool detectClockRollover();
+  private:
+	// 当系统时间改变时 -> 调用该函数
+	bool detectClockRollover();
 
-    private:
-        std::shared_mutex m_mutex;
+  private:
+	std::shared_mutex m_mutex;
 
-        // 时间堆
-        std::set<std::shared_ptr<Timer>, Timer::Comparator> m_timers;
-        // 在下次getNextTime()执行前 onTimerInsertedAtFront()是否已经被触发了 -> 在此过程中 onTimerInsertedAtFront()只执行一次
-        bool m_tickled = false;
-        // 上次检查系统时间是否回退的绝对时间
-        std::chrono::time_point<std::chrono::system_clock> m_previouseTime;
-    };
-}
+	// 时间堆
+	std::set<std::shared_ptr<Timer>, Timer::Comparator> m_timers;
+	// 在下次getNextTime()执行前 onTimerInsertedAtFront()是否已经被触发了 ->
+	// 在此过程中 onTimerInsertedAtFront()只执行一次
+	bool m_tickled = false;
+	// 上次检查系统时间是否回退的绝对时间
+	std::chrono::time_point<std::chrono::system_clock> m_previouseTime;
+};
+} // namespace moczkrin
